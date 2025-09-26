30e3cc3c85aaed41d0c5d44c646edaa075ba608c
ShocKWavEv1 <rodd.sal23@gmail.com>
2025-09-26 17:38:04 -0600

    feat: Base app with fonts, working with nuxt 4 and tailwind 4

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="w-full h-[100svh] flex flex-col gap-[12px] items-center justify-center bg-zinc-950"
+  >
+    <NuxtRouteAnnouncer />
+    <h1 class="text-9xl text-white font-headlines font-[300]">
+      Centro Holístico
+    </h1>
+    <h1 class="text-2xl font-lettra">NUXT 4 & TailwindCSS 4</h1>
+  </div>
+</template>
