0a15aa70b1fa2110891480a32612e249cbb8078c
ShocKWavEv1 <rodd.sal23@gmail.com>
2025-09-26 17:55:23 -0600

    feat: preload all fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,78 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  app: {
+    head: {
+      link: [
+        // PPLettraMono
+        {
+          rel: "preload",
+          as: "font",
+          href: "/fonts/museum/PPLettraMono-Ultralight.woff2",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "preload",
+          as: "font",
+          href: "/fonts/museum/PPLettraMono-Regular.woff2",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "preload",
+          as: "font",
+          href: "/fonts/museum/PPLettraMono-Medium.woff2",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "preload",
+          as: "font",
+          href: "/fonts/museum/PPLettraMono-Ultrabold.woff2",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+
+        // PPRadioGrotesk
+        {
+          rel: "preload",
+          as: "font",
+          href: "/fonts/radio/PPRadioGrotesk-Ultralight.woff2",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "preload",
+          as: "font",
+          href: "/fonts/radio/PPRadioGrotesk-Light.woff2",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "preload",
+          as: "font",
+          href: "/fonts/radio/PPRadioGrotesk-Regular.woff2",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "preload",
+          as: "font",
+          href: "/fonts/radio/PPRadioGrotesk-Bold.woff2",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "preload",
+          as: "font",
+          href: "/fonts/radio/PPRadioGrotesk-Black.woff2",
+          type: "font/woff2",
+          crossorigin: "anonymous",
+        },
+      ],
+    },
+  },
   modules: ["@nuxt/eslint", "@nuxt/image"],
   css: ["~/assets/css/main.css"],
   vite: {
